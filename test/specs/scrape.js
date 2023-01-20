148ee73583f937678f55d58379116238d1d711a5
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-01-20 20:01:31 +0000

    mapped title & info

--- test/specs/scrape.js
+++ test/specs/scrape.js
@@ -2,23 +2,26 @@ describe("Company details", () => {
     it("get company, name contact details & logo", async () => {
         browser.url("https://www.medicines.org.uk/emc/company/13");
         await $("#onetrust-accept-btn-handler").click();
+
+        ///********** to use
         // asigns element to variable then logs text
         //const title = await $('h1');
+        // below needs to be pushed to company array when all details are ready
        //console.log("Here:" , await title.getText());
 
-
-       //can use $$ to get array but can't .get text of more than 1 at the moment 
+        //const Company = [];
         const contactHeadings = await $$('.gfdCompanyDetailsTitle');
         const headingText = [];
         const companyDetails = await $$('.gfdCompanyDetailsCol p');
         const companyInformation = [];
-        console.log("HERE:", companyDetails);
+        let contactMap = new Map();
+        //console.log("HERE:", companyDetails);
         
          //********** to use
         //loops & pushes the titles to new variable 
-        // for (var contact of contactHeadings ) {
-        //     headingText.push(await contact.getText());
-        // }
+        for (var contact of contactHeadings ) {
+            headingText.push(await contact.getText());
+        }
 
         //console.log("HERE:", headingText);
 
@@ -28,6 +31,13 @@ describe("Company details", () => {
             companyInformation.push(await company.getText());
         }
 
-        console.log("HERE:", companyInformation);
+        for (var h = 0; h < headingText.length; h ++ ) {
+            contactMap.set(headingText[h], companyInformation[h]);
+        }
+        console.log("HERE:", contactMap);
+
+        //console.log("HERE:", companyInformation);
+    
+
     })
 })
