cacf3d34d24565a73eb3edd8a77ad3d4b040578e
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-01-20 12:24:01 +0000

    object update

--- test/specs/index.js
+++ test/specs/index.js
@@ -3,18 +3,16 @@ const fs = require('fs');
 const newObject = {
     "name": "Mega corp", 
     "address": " 52 reesher road, fr4 89e",
-    "time": "9am"
+    "number": "9"
 }
 //const jsonString = JSON.stringify(newObject);
 
-fs.writeFile('./test/data/companies.json', JSON.stringify(newObject), err => {
-    if (err) {
-        console.log(err);
-        } else {
-            console.log('File successfully written!');
-        }
-});
+// fs.writeFile('./test/data/companies.json', JSON.stringify(newObject, null, 2), err => {
+//     if (err) {
+//         console.log(err);
+//         } else {
+//             console.log('File successfully written!');
+//         }
+// });
 
-
-//const company = require('companies.json')
 
