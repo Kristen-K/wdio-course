33c6b893e73b374aa8ea562f46f0119e92b92675
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-26 16:04:00 +0000

    linted

--- test/specs/index.js
+++ test/specs/index.js
@@ -1,119 +1,21 @@
 const fs = require("fs");
 
-// helper function (time 5.50)
-// function jsonReader(filePath, cb) {
-//     fs.readFile(filePath, 'utf-8', (err, fileData) => {
-//         if (err) {
-//             return cb && cb(err);
-//         } try {
-//             const object = JSON.parse(fileData);
-//             return cb & cb(null, object);
-//         } catch (err) {
-//             return cb & cb(err);
-//         }
-//     })
-// }
-
-//created oject/ updatd object
-// const anotherObject = {
-//     name: 'Jack Hint',
-//     job_count: '3',
-//     postcode: 'kr6 7ic'
-// }
-
-//const jsonString = JSON.stringify(anotherObject);
-//console.log(jsonString);
-
-// used helper function to add 1 to count, also added job_count
-// jsonReader('./test/data/companies.json', (err, data) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         data.job_count += 1;
-//         data.name = "keeper",
-//         //data.job_count =  6,
-//         fs.writeFile('./test/data/companies.json', JSON.stringify(data), err => {
-//             if (err) {
-//                 console.log(err);
-//             }
-//          })
-//      }
-// })
-
-// executes jsonReader helper function
-// jsonReader('./test/data/companies.json', (err, data) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log(data.name);
-//     }
-// })
-
-// async reads file method
-// fs.readFile('./test/data/companies.json', 'utf-8', (err, jsonString) => {
-//   if (err)
-//  {
-//     console.log(err);
-//  } else {
-//     try {
-//         const data = JSON.parse(jsonString);
-//         console.log(data.address);
-//     } catch (err) {
-//         console.log('Error parsing JSON', err)
-//     }
-//  }
-// });
-
-//reated oject/ updatd object
+//created object/ updated
 const newObject = {
   name: "big pool",
   address: " 52 reesher road, fr4 89e",
   number: "9",
 };
 
-// fs.readFile("./test/data/companies.json", function (err, data) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     var json = JSON.parse(data);
-//     const newJSON = Object.assign(json, newObject);
-//     fs.writeFile(
-//       "./test/data/companies.json",
-//       JSON.stringify(newJSON, null, 2),
-//       (err) => {
-//         console.log(err);
-//       }
-//     );
-//   }
-// });
-
-// const file = fs.readFile('./test/data/donkey.json');
-
-// //check if file is empty
-// if (file.length == 0) {
-//     //add data to json file
-//     fs.writeFile('./test/data/donkey.json', JSON.stringify(newObject, null, 2), err => {
-//         if (err) {
-//             console.log(err);
-//             } else {
-//                 console.log('File successfully written!');
-//             }
-//     });
-//}
-
-// 7.22
-
-//let data = JSON.parse('[' + newObject.join(',' + ']'))
-
-//console.log(data);
-
-fs.writeFileSync('./test/data/donkey.json', JSON.stringify(newObject, null, 2),{flag:'a+'}, err => {
+fs.writeFileSync(
+  "./test/data/donkey.json",
+  JSON.stringify(newObject, null, 2),
+  { flag: "a+" },
+  (err) => {
     if (err) {
-        console.log(err);
-        } else {
-            console.log('File successfully written!');
-        }
-});
-
-
-// add to [ array] and commas
+      console.log(err);
+    } else {
+      console.log("File successfully written!");
+    }
+  }
+);
--- test/specs/logos.js
+++ test/specs/logos.js
@@ -1,30 +1,13 @@
 describe("Company details", () => {
-    it("get company, name contact details & logo", async () => {
-        browser.url("https://www.medicines.org.uk/emc/company/13");
-        await $("#onetrust-accept-btn-handler").click();
+  it("get company, name contact details & logo", async () => {
+    browser.url("https://www.medicines.org.uk/emc/company/13");
+    await $("#onetrust-accept-btn-handler").click();
 
-        let title = await $('h1').getText();
-        console.log(title);
+    let title = await $("h1").getText();
+    console.log(title);
 
-
-        // set path as a variable? how
-        let path = `test/data/logos/${title}.png`;
-       const logos = await $('.img-responsive')
-       await logos.saveScreenshot(path);
-    
-       //console.log("HERE:", logos);
-    
-
-    })
-})
-
-
-
-
- // to try out wrong - need to add path of where to save not href on logos 
- // create direct then save each file name as title
-//  const logos = await $('.img-responsive').getAttribute('src');
-//  await logos.saveScreenshot('test/data/logos/name.png');
-
- // try something like thsi ${title} test/data/logos/${title}
-    
+    let path = `test/data/logos/${title}.png`;
+    const logos = await $(".img-responsive");
+    await logos.saveScreenshot(path);
+  });
+});
