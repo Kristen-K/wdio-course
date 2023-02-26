8ca6fb3f898f07c936dbccc93e630838127295a0
Kristen-K <42575870+Kristen-K@users.noreply.github.com>
2023-02-26 15:59:57 +0000

    linted

--- test/specs/iframe.js
+++ test/specs/iframe.js
@@ -1,20 +1,19 @@
-describe('IFrame', () => {
-it('Working with iframe', async () => {
+describe("IFrame", () => {
+  it("Working with iframe", async () => {
     // open url
-    await browser.url('/iframe-sample/')
+    await browser.url("/iframe-sample/");
 
     // access the iframe
-    const iframe = await $('#advanced_iframe'); 
+    const iframe = await $("#advanced_iframe");
     await browser.switchToFrame(iframe);
 
     // verify logo exists
-    await expect($('#site-logo')).toExist();
+    await expect($("#site-logo")).toExist();
 
     // switch to parent site
     await browser.switchToParentFrame();
 
     // verify page title
-    await expect($('h1.tg-page-header__title')).toHaveText('IFrame Sample');
-
-    });
-})
+    await expect($("h1.tg-page-header__title")).toHaveText("IFrame Sample");
+  });
+});
--- test/specs/nav.js
+++ test/specs/nav.js
@@ -1,52 +1,41 @@
-import HomePage from '../pages/home-page';
-import allureReporter from '@wdio/allure-reporter';
+import HomePage from "../pages/home-page";
+import allureReporter from "@wdio/allure-reporter";
 
-describe('Navigation Menu', () => {
-    it('Get the text of all menu items & assert them', async () => {
-        allureReporter.addFeature("Navigation");
-        allureReporter.addSeverity("critical");
-        await HomePage.open();
+describe("Navigation Menu", () => {
+  it("Get the text of all menu items & assert them", async () => {
+    allureReporter.addFeature("Navigation");
+    allureReporter.addSeverity("critical");
+    await HomePage.open();
 
-        const expectedLinks = [
-            "Home", 
-            "About", 
-            "Shop",
-            "Blog",
-            "Contact",
-            "My account",
-        ];
+    const expectedLinks = [
+      "Home",
+      "About",
+      "Shop",
+      "Blog",
+      "Contact",
+      "My account",
+    ];
 
-        const actualLinks = [];
+    const actualLinks = [];
 
-        // wait until Home text is displayed on the navigation menu
-        await browser.waitUntil(async function () {
-            const homeText = await HomePage.NavComponent.firstNavMenu.getText(); //Home
-            return homeText === "Home"; // true | false
-        }, {
-            timeout: 5000,
-            timeoutMsg: 'Could not verify the Home text from #primary-menu li'
-        });
+    // wait until Home text is displayed on the navigation menu
+    await browser.waitUntil(
+      async function () {
+        const homeText = await HomePage.NavComponent.firstNavMenu.getText(); //Home
+        return homeText === "Home"; // true | false
+      },
+      {
+        timeout: 5000,
+        timeoutMsg: "Could not verify the Home text from #primary-menu li",
+      }
+    );
 
-        // wait until Home text is displayed on the navigation menu
-       // await $('#primary-menu li').waitForDisplayed();
+    const navLinks = await HomePage.NavComponent.linksNavMenu;
 
-        // wait until Home link to be clickable in the navigation menu
-        //await $('#primary-menu li').waitForClickable()
+    for (const link of navLinks) {
+      actualLinks.push(await link.getText());
+    }
 
-        //Verify if Home element is disabled
-       // await $('#primary-menu li').waitForEnabled({reverse: true, timeoutMsg: 'Could not verify #primary-menu li is disabled'})
-
-
-        // alt if want to change  const navLinks = await $('#primary-menu').$$('li[id*=menu]');
-        const navLinks = await HomePage.NavComponent.linksNavMenu;
-
-        for (const link of navLinks) {
-            actualLinks.push(await link.getText());
-        }
-
-        await expect(expectedLinks).toEqual(actualLinks);
-
-
-       
-    })
+    await expect(expectedLinks).toEqual(actualLinks);
+  });
 });
